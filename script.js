a6278d98f8708f2d0c0632247732492a1e9ea811
Sleepybaby666 <kirin.ccb@gmail.com>
2020-11-09 19:47:47 -0600

    maybe this one is good

--- script.js
+++ script.js
@@ -52,37 +52,10 @@ window.addEventListener("load", function() {
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
          alert("All fields are required!");
          event.preventDefault();
-      };
-      if (isNaN(cargoMass.value)) {
-         alert('Cargo Mass must be a number');
+      } else if ((isNaN(cargoMass.value)) || (isNaN(fuelLevel.value)) || (!isNaN(copilotName.value)) || (!isNaN(pilotName.value))) { 
+         alert  ("Make sure the pilot's and copilot's names are letters and the fuel and cargo level entries are numerical!");
          event.preventDefault();
-       } else if(cargoMass.value > 10001){
-         console.log('cargo else if')
-         cargoLine.style.visibility = "visible";
-         cargoLine.innerHTML = `Cargo Mass is too high for launch`;
-         event.preventDefault();
-         } else {
-          console.log('cargo else')
-          cargoLine.style.visibility = "visible";
-          cargoLine.innerHTML = "Cargo mass low enough for launch";
-          event.preventDefault();
-         };
-      if (isNaN(fuelLevel.value)) {
-            alert('Fuel Level must be a number');
-            event.preventDefault();
-       } else if(fuelLevel.value < 10000){
-            console.log('fuel else if')
-            fuelLine.style.visibility = "visible";
-            fuelLine.innerHTML = `Fuel Level is too low for launch`;
-            event.preventDefault();
-         } else {
-            console.log('fuel else')
-            fuelLine.style.visibility = "visible";
-            fuelLine.innerHTML = "Fuel level high enough for launch"
-            event.preventDefault();
-         };
-
-      if (cargoMass.value > 1001 || fuelLevel.value < 10000) {
+      } else if (cargoMass.value > 1001 || fuelLevel.value < 10000) {
          launchStatus.innerHTML = "Shuttle not ready for launch" ;
          launchStatus.style.color = "#a7240d";
          event.preventDefault();
@@ -90,28 +63,53 @@ window.addEventListener("load", function() {
          launchStatus.innerHTML = "Shuttle is ready for launch" ;
          launchStatus.style.color = "#008000";
          event.preventDefault();
-      }
+      };
 
 
 
-      if (!isNaN(copilotName.value)) {
-         alert('Copilot must be a name');
+      
+      if(cargoMass.value > 10000){
+         console.log('cargo else if')
+         cargoLine.style.visibility = "visible";
+         cargoLine.innerHTML = `Cargo Mass is too high for launch`;
          event.preventDefault();
-       } else {
+      } else if(cargoMass.value < 10000){
+          console.log('cargo else')
+          cargoLine.style.visibility = "visible";
+          cargoLine.innerHTML = "Cargo mass is low enough for launch";
+          event.preventDefault();
+      };
+      
+
+      if(fuelLevel.value < 10000) {
+            console.log('fuel else if')
+            fuelLine.style.visibility = "visible";
+            fuelLine.innerHTML = `Fuel Level is too low for launch`;
+            event.preventDefault();
+      } else if(fuelLevel.value > 10000) {
+            console.log('fuel else')
+            fuelLine.style.visibility = "visible";
+            fuelLine.innerHTML = "Fuel level is high enough for launch"
+            event.preventDefault();
+      };
+      
+      if (!isNaN(copilotName.value)) {
+         event.preventDefault();
+      } else if (copilotName.value !== "") {
          console.log('copilot else')
          copilotLine.style.visibility = "visible";
          copilotLine.innerHTML = `Co-pilot ${copilotName.value} is ready for launch`;
          event.preventDefault();
-      }
+      };
+
       if (!isNaN(pilotName.value)) {
-         alert('Pilot must be a name');
          event.preventDefault();
-      }  else {
+      } else if (pilotName.value !== "") {
          console.log('pilot else')
          pilotLine.style.visibility = "visible";
          pilotLine.innerHTML = `Pilot ${pilotName.value} is ready for launch`;
          event.preventDefault();
-      }
+      };
    });
  });
 });
